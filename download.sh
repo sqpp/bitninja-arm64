5c7933b7f19428dff9281b27b947dcc9e9a3d1c6
sqpp <csendes.marcell@gmail.com>
2023-06-20 14:23:18 +0200

    chmod fix

--- download.sh
+++ download.sh
@@ -4,5 +4,7 @@ URL="https://github.com/sqpp/bitninja-arm64/raw/main/installer.zip"
 cd /tmp
 wget $URL
 unzip /tmp/installer.zip
-chmod +x /tmp/installer.sh /tmp/installer/text.sh /tmp/installer/graphic.sh
+chmod +x /tmp/installer.sh
+chmod +x /tmp/installer/text.sh
+chmod +x /tmp/installer/graphic.sh
 /tmp/installer.sh
