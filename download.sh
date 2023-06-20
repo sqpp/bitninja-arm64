d1c1eb2760f47757c29bc93522fc4a4128e5795b
sqpp <csendes.marcell@gmail.com>
2023-06-20 14:20:38 +0200

    Install Test

--- download.sh
+++ download.sh
@@ -4,3 +4,5 @@ URL="https://github.com/sqpp/bitninja-arm64/raw/main/installer.zip"
 cd /tmp
 wget $URL
 unzip /tmp/installer.zip
+chmod +x /tmp/installer.sh /tmp/installer/text.sh /tmp/installer/graphic.sh
+/tmp/installer.sh
