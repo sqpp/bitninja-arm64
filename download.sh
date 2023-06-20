ad95c59299a37ff1bdfb7678946604d9026873c0
sqpp <csendes.marcell@gmail.com>
2023-06-20 14:46:09 +0200

    Final download fix

--- download.sh
+++ download.sh
@@ -2,9 +2,8 @@
 
 URL="https://github.com/sqpp/bitninja-arm64/raw/main/installer.zip"
 wget $URL -O /tmp/installer.zip
-unzip -oq /tmp/installer.zip
+unzip -q installer.zip -d /tmp
 chmod +x /tmp/installer.sh
 chmod +x /tmp/installer/text.sh
 chmod +x /tmp/installer/graphic.sh
 ./tmp/installer.sh
-rm -rf /tmp/installer*
