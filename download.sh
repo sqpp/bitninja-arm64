67db793909d8e5b975151df74557bf047f642dcb
sqpp <csendes.marcell@gmail.com>
2023-06-20 15:58:27 +0200

    Confirmed final fix

--- download.sh
+++ download.sh
@@ -5,7 +5,8 @@ temp_dir=$(mktemp -d)
 
 # Download the installer.zip file
 if command -v curl >/dev/null 2>&1; then
-    curl -o "$temp_dir/files.tar.gz" "$URL";
+    cd $temp_dir && curl -L $URL | tar zx
+    echo "Downloaded file size: $file_size bytes"
 elif command -v wget >/dev/null 2>&1; then
     wget -q "$URL" -O "$temp_dir/files.tar.gz"
 else
@@ -13,21 +14,13 @@ else
     exit 1
 fi
 
-# Print the contents of the temporary directory
-ls -l "$temp_dir"
-
-tar -xf "$temp_dir/files.tar.gz" -C "$temp_dir/"
-
 # Set the correct installer directory path
 installer_dir="$temp_dir"
 
-# Print the contents of the installer directory
-ls -l "$installer_dir"
-
 # Set permissions for the installer.sh and other scripts
 chmod +x "$installer_dir/installer.sh"
-chmod +x "$installer_dir/text.sh"
-chmod +x "$installer_dir/graphic.sh"
+chmod +x "$installer_dir/installer/text.sh"
+chmod +x "$installer_dir/installer/graphic.sh"
 
 # Run the installer.sh script
 "$installer_dir/installer.sh"
